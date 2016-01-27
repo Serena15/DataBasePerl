3a615f7ffff304be35148484b0903a4accb83eac
Serena15 <raketa9390@mail.ru>
2016-01-27 17:24:00 +0300

    Update Teacher.pm

--- grades/lib/Local/Schema/ResultSet/Teacher.pm
+++ grades/lib/Local/Schema/ResultSet/Teacher.pm
@@ -5,14 +5,12 @@ sub search_by_name {
   my $self = shift;
   my $name = shift;
   $name = "%".$name."%";
-  return $self->search(
-  {
+  return $self->search( {
     -or => [
     'first_name' => {like => $name},
     'last_name'  => {like => $name},
     ]
-  }
-    );
+  } );
 }
 
 1;
