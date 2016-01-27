f393f870cf52066d340d9999ae59c1421203d49d
Serena15 <raketa9390@mail.ru>
2016-01-27 17:23:22 +0300

    Update Grade.pm

--- grades/lib/Local/Schema/ResultSet/Grade.pm
+++ grades/lib/Local/Schema/ResultSet/Grade.pm
@@ -4,9 +4,8 @@ use DDP;
 
 sub max_points {
     my $self = shift;   
-    my @rs = $self->search(undef, { order_by => {-desc => 'points'} })->all();
-    my $first_max = $rs[0]->points;
-    return $first_max;
+    my @rs = $self->search(undef, { order_by => {-desc => 'points'} })->next;
+    return $rs[0]->points;
 }
 
 1;
